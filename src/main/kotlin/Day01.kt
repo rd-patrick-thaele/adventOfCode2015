88eb7f4de0bd64100de08c9ab27dc049d4993877
Patrick Thäle <patrick.thaele@paymenttools.com>
2023-11-30 20:53:43 +0100

    :construction: Day01 Part 1

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,6 +1,9 @@
-class Day01 {
+class Day01(private val directions: String) {
 
-    fun getHelloWorld(): String {
-        return "Hello World"
+    fun floor(): Int{
+        val goUp = directions.count { it == '(' }
+        val goDown = directions.length - goUp
+
+        return goUp - goDown
     }
 }
--- src/test/kotlin/Day01Test.kt
+++ src/test/kotlin/Day01Test.kt
@@ -4,19 +4,35 @@ import io.kotest.matchers.shouldBe
 class Day01Test : FreeSpec({
 
     "samples" - {
-        "helloWorld Test" {
-            Day01().getHelloWorld() shouldBe "Hello World"
+        "ground floor" {
+            Day01("(())").floor() shouldBe 0
+            Day01("()()").floor() shouldBe 0
+        }
+
+        "3rd floor" {
+            Day01("(((").floor() shouldBe 3
+            Day01("(()(()(").floor() shouldBe 3
+            Day01("))(((((").floor() shouldBe 3
+        }
+
+        "basement level" {
+            Day01("())").floor() shouldBe -1
+            Day01("))(").floor() shouldBe -1
+        }
+
+        "underground" {
+            Day01(")))").floor() shouldBe -3
+            Day01(")())())").floor() shouldBe -3
         }
     }
 
     "solution" - {
         "part 1" {
             // given
-
-            // when
+            val directions = getResourceFileAsStringSequence("day01/input_part1.txt")
 
             // then
-            1 + 1 shouldBe 2
+            Day01(directions.first()).floor() shouldBe 74
         }
     }
 }) {
