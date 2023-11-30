ee843cae5e6047eadca4b362c89174aa02549af3
Patrick Thäle <patrick.thaele@paymenttools.com>
2023-11-30 22:32:07 +0100

    :construction: Day 02 part 1

---
+++ src/main/kotlin/Day02.kt
@@ -0,0 +1,28 @@
+class Day02 {
+    fun getTotalWrappingPaper(elvesList: List<String>): Int {
+        return readElvesList(elvesList).map { it.neededWrappingPaper() }
+            .sum()
+    }
+
+    fun readElvesList(elvesList: List<String>): List<Present> {
+        val boxRegex = "([0-9]+)x([0-9]+)x([0-9]+)".toRegex()
+
+        return elvesList.map {
+            val (length, width, height) = boxRegex.find(it)!!.destructured
+            Present(length.toInt(), width.toInt(), height.toInt())
+        }
+    }
+}
+
+data class Present(val length: Int, val width: Int, val height: Int) {
+    fun neededWrappingPaper(): Int{
+        val area1 = length * width
+        val area2 = length * height
+        val area3 = width * height
+
+        val slackMaterial = listOf(area1, area2, area3).min()
+
+        return 2 * area1 + 2 * area2 + 2 * area3 + slackMaterial
+    }
+
+}
---
+++ src/test/kotlin/Day02Test.kt
@@ -0,0 +1,57 @@
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.shouldBe
+
+class Day02Test : FreeSpec({
+
+    "samples" - {
+        "present wrapping paper" {
+            Present(2, 3, 4).neededWrappingPaper() shouldBe 58
+            Present(1, 1, 10).neededWrappingPaper() shouldBe 43
+        }
+
+        "read elves list" {
+            // given
+            val elvesList = listOf("2x3x4","1x1x10")
+
+            // when
+            val presents = Day02().readElvesList(elvesList)
+
+            // expected
+            presents shouldContain Present(2,3,4)
+            presents shouldContain Present(1,1,10)
+            presents.size shouldBe 2
+        }
+
+        "part 1 - total wrapping paper" - {
+            // given
+            val elvesList = listOf("2x3x4","1x1x10")
+
+            // when
+            val totalWrappingPaper = Day02().getTotalWrappingPaper(elvesList)
+
+            // then
+            totalWrappingPaper shouldBe 101
+        }
+
+    }
+
+    "solution" - {
+
+        "part 1" {
+            // given
+            val elvesList = getResourceFileAsStringSequence("day02/input.txt")
+
+            // when
+            val totalWrappingPaper = Day02().getTotalWrappingPaper(elvesList)
+
+            // then
+            totalWrappingPaper shouldBe 1588178
+        }
+
+        "part 2" {
+
+        }
+    }
+}) {
+}
