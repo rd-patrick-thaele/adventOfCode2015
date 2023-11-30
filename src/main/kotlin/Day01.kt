7168da9fa9a0ef6707ae5319acce52becddc5725
Patrick Thäle <patrick.thaele@paymenttools.com>
2023-11-30 21:08:32 +0100

    :bookmark: Day01 Part 2

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -1,9 +1,29 @@
 class Day01(private val directions: String) {
 
+    private val BASEMENT = -1
+
     fun floor(): Int{
         val goUp = directions.count { it == '(' }
         val goDown = directions.length - goUp
 
         return goUp - goDown
+    }
+
+    fun nbOfFloorsPassedWhenArrivingAtBasementTheFirstTime(): Int {
+        var floor = 0
+        var nbOfFloorsPassed = 0
+
+        for (direction: Char in directions) {
+            nbOfFloorsPassed++
+
+            when (direction) {
+                '(' -> floor++
+                ')' -> floor--
+            }
+
+            if (floor == BASEMENT) break
+        }
+
+        return nbOfFloorsPassed
     }
 }
--- src/test/kotlin/Day01Test.kt
+++ src/test/kotlin/Day01Test.kt
@@ -4,35 +4,53 @@ import io.kotest.matchers.shouldBe
 class Day01Test : FreeSpec({
 
     "samples" - {
-        "ground floor" {
-            Day01("(())").floor() shouldBe 0
-            Day01("()()").floor() shouldBe 0
+        "part 1" - {
+            "ground floor" {
+                Day01("(())").floor() shouldBe 0
+                Day01("()()").floor() shouldBe 0
+            }
+
+            "3rd floor" {
+                Day01("(((").floor() shouldBe 3
+                Day01("(()(()(").floor() shouldBe 3
+                Day01("))(((((").floor() shouldBe 3
+            }
+
+            "basement level" {
+                Day01("())").floor() shouldBe -1
+                Day01("))(").floor() shouldBe -1
+            }
+
+            "underground" {
+                Day01(")))").floor() shouldBe -3
+                Day01(")())())").floor() shouldBe -3
+            }
         }
 
-        "3rd floor" {
-            Day01("(((").floor() shouldBe 3
-            Day01("(()(()(").floor() shouldBe 3
-            Day01("))(((((").floor() shouldBe 3
+        "part 2" - {
+            "enters basement at 1" {
+                Day01(")").nbOfFloorsPassedWhenArrivingAtBasementTheFirstTime() shouldBe 1
+            }
+
+            "enters basement at 5" {
+                Day01("()())").nbOfFloorsPassedWhenArrivingAtBasementTheFirstTime() shouldBe 5
+            }
         }
 
-        "basement level" {
-            Day01("())").floor() shouldBe -1
-            Day01("))(").floor() shouldBe -1
-        }
-
-        "underground" {
-            Day01(")))").floor() shouldBe -3
-            Day01(")())())").floor() shouldBe -3
-        }
     }
 
     "solution" - {
-        "part 1" {
-            // given
-            val directions = getResourceFileAsStringSequence("day01/input_part1.txt")
+        // given
+        val directions = getResourceFileAsStringSequence("day01/input_part1.txt")
 
+        "part 1" {
             // then
             Day01(directions.first()).floor() shouldBe 74
+        }
+
+        "part 2" {
+            // then
+            Day01(directions.first()).nbOfFloorsPassedWhenArrivingAtBasementTheFirstTime() shouldBe 1795
         }
     }
 }) {
