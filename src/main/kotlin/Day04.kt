fdbc75908ac4ced443486a7841cb7fddbb20bc04
Patrick Thäle <patrick.thaele@paymenttools.com>
2023-12-01 21:40:42 +0100

    :recycle: Extend parameters for mining

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -11,13 +11,13 @@ class Day04 {
             .padStart(32, '0')
     }
 
-    fun mineAdventCoins(secret: String): Int {
+    fun mineAdventCoins(secret: String, nbOfZeros: Int = 5): Int {
         var seed = 0
 
         while(true) {
           val md5 = getMd5InHex(secret + seed)
 
-          if (md5.startsWith("00000")) return seed
+          if (md5.startsWith("0".repeat(nbOfZeros))) return seed
 
           seed++
         }
